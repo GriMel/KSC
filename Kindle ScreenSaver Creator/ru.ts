b05dc63d0e247ed8af946d540a2be9a0e00a888f
GriMel <GriefMontana@gmail.com>
2014-09-26 21:20:57 +0300

    added some resources

---
+++ Kindle ScreenSaver Creator/ru.ts
@@ -0,0 +1,135 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru">
+<context>
+    <name>CropDialog</name>
+    <message>
+        <location filename="aksc.py" line="122"/>
+        <source>Crop Image</source>
+        <translation type="unfinished">Подгонка изображения</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="123"/>
+        <source>Choose version of Kindle</source>
+        <translation type="unfinished">Выберите версию Kindle</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="124"/>
+        <source>Default</source>
+        <translation type="unfinished">По умолчанию</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="131"/>
+        <source>Crop</source>
+        <translation type="unfinished">Обрезать</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="132"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <location filename="aksc.py" line="505"/>
+        <source>Open...</source>
+        <translation type="unfinished">Открыть...</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="506"/>
+        <source>Name</source>
+        <translation type="unfinished">Имя</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="507"/>
+        <source>Surname</source>
+        <translation type="unfinished">Фамилия</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="508"/>
+        <source>White</source>
+        <translation type="unfinished">Белый</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="509"/>
+        <source>Black</source>
+        <translation type="unfinished">Черный</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="510"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Text color&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Цвет текста&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="511"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Where to place text&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;В каком углу поместить текст&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="512"/>
+        <source>Left Upper</source>
+        <translation type="unfinished">Левый верхний</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="513"/>
+        <source>Right Lower</source>
+        <translation type="unfinished">Правый нижний</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="514"/>
+        <source>Left Lower</source>
+        <translation type="unfinished">Левый нижний</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="515"/>
+        <source>Right Upper</source>
+        <translation type="unfinished">Правый верхний</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="518"/>
+        <source>Create</source>
+        <translation type="unfinished">Создать</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="527"/>
+        <source>Main</source>
+        <translation type="unfinished">Главное меню</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="528"/>
+        <source>Exit</source>
+        <translation type="unfinished">Выход</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="529"/>
+        <source>Language</source>
+        <translation type="unfinished">Язык</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="653"/>
+        <source>Open file</source>
+        <translation type="unfinished">Открыть файл</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="658"/>
+        <source>No image</source>
+        <translation type="unfinished">Неверный формат изображения</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="714"/>
+        <source>Opened file</source>
+        <translation type="unfinished">Файл открыт</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="723"/>
+        <source>Sample</source>
+        <translation type="unfinished">Пример</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="742"/>
+        <source>File {} created!</source>
+        <translation type="unfinished">Файл {} создан!</translation>
+    </message>
+</context>
+</TS>
---
+++ Kindle ScreenSaver Creator/ukr.ts
@@ -0,0 +1,135 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="uk">
+<context>
+    <name>CropDialog</name>
+    <message>
+        <location filename="aksc.py" line="122"/>
+        <source>Crop Image</source>
+        <translation type="unfinished">Підгонка зображення</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="123"/>
+        <source>Choose version of Kindle</source>
+        <translation type="unfinished">Оберіть версію Kindle</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="124"/>
+        <source>Default</source>
+        <translation type="unfinished">За замовчуванням</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="131"/>
+        <source>Crop</source>
+        <translation type="unfinished">Обрiзати</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="132"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <location filename="aksc.py" line="505"/>
+        <source>Open...</source>
+        <translation type="unfinished">Вiдкрити...</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="506"/>
+        <source>Name</source>
+        <translation type="unfinished">Iм&apos;я</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="507"/>
+        <source>Surname</source>
+        <translation type="unfinished">Прiзвище</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="508"/>
+        <source>White</source>
+        <translation type="unfinished">Бiлий</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="509"/>
+        <source>Black</source>
+        <translation type="unfinished">Чорний</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="510"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Text color&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Колiр тексту&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="511"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Where to place text&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;В якому кутку помiстити текст&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="512"/>
+        <source>Left Upper</source>
+        <translation type="unfinished">Лiвий верхнiй</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="513"/>
+        <source>Right Lower</source>
+        <translation type="unfinished">Правий нижнiй</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="514"/>
+        <source>Left Lower</source>
+        <translation type="unfinished">Лiвий нижнiй</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="515"/>
+        <source>Right Upper</source>
+        <translation type="unfinished">Правий верхнiй</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="518"/>
+        <source>Create</source>
+        <translation type="unfinished">Створити</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="527"/>
+        <source>Main</source>
+        <translation type="unfinished">Головне меню</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="528"/>
+        <source>Exit</source>
+        <translation type="unfinished">Вихiд</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="529"/>
+        <source>Language</source>
+        <translation type="unfinished">Мова</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="653"/>
+        <source>Open file</source>
+        <translation type="unfinished">Вiдкрити файл</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="658"/>
+        <source>No image</source>
+        <translation type="unfinished">Невiрний формат зображення</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="714"/>
+        <source>Opened file</source>
+        <translation type="unfinished">Файл вiдкрито</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="723"/>
+        <source>Sample</source>
+        <translation type="unfinished">Приклад</translation>
+    </message>
+    <message>
+        <location filename="aksc.py" line="742"/>
+        <source>File {} created!</source>
+        <translation type="unfinished">Файл {} створено!</translation>
+    </message>
+</context>
+</TS>
